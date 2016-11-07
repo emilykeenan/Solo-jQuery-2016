2060cbf97bfb59f8643b002a6133a046fa817e69
emilykeenan <emilyjanekeenan@gmail.com>
2016-11-07 15:40:45 -0600

    counters working

--- app.js
+++ app.js
@@ -1,16 +1,46 @@
-//function for adding to total boxes
-//function for appending box to DOM
-
-var count = 0;
+//declaring initial color count variables, set at 0
+var redCount = 0;
+var yellowCount = 0;
+var greenCount = 0;
+var blueCount = 0;
 
 $( document ).ready(function() {
-  $(".color-button").on("click", addToDom);
 
+  //click listeners for color-button
+  $(".color-button").on("click", addToDom);
+  $(".color-button").on("click", addTotalColor);
 });
 
+
+//function to add colored div to the DOM
 function addToDom() {
+  //declaring color variable
   var color = $(this).data("color");
   $(".container").append("<div class='color-cube " + color + "'></div>");
-  count++;
-  $("#" + color).empty().text()
+}
+
+//function to add to total color count
+function addTotalColor() {
+
+  //delclaring color variable
+  var color = $(this).data("color");
+
+  //cycling through to add to correct color total
+  if (color == "red") {
+    redCount++;
+    $("#" + color).text("Total " + color + ": " + redCount);
+
+  } else if (color == "yellow") {
+    yellowCount++;
+    $("#" + color).text("Total " + color + ": " + yellowCount);
+    
+  } else if (color == "green") {
+    greenCount++;
+    $("#" + color).text("Total " + color + ": " + greenCount);
+
+  } else {
+    blueCount++;
+    $("#" + color).text("Total " + color + ": " + blueCount);
+
+  }
 }
