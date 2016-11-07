6cbb23853298cbc2ce0044545f68a572b72efb84
emilykeenan <emilyjanekeenan@gmail.com>
2016-11-07 15:50:26 -0600

    switched to switch statement and added comments

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
-//declaring initial color count variables, set at 0
+//declaring initial global color count variables, set at 0 to start
 var redCount = 0;
 var yellowCount = 0;
 var greenCount = 0;
 var blueCount = 0;
+
+var color; //declaring global color variable so it is available to both functions
 
 $( document ).ready(function() {
 
@@ -15,32 +17,29 @@ $( document ).ready(function() {
 //function to add colored div to the DOM
 function addToDom() {
   //declaring color variable
-  var color = $(this).data("color");
+  color = $(this).data("color");
   $(".container").append("<div class='color-cube " + color + "'></div>");
 }
 
-//function to add to total color count
+//function to add to total color count, cycling through four colors and adding to total of appropriate color
+//there is no need to redeclare the color variable as it will already be available from addToDom function
 function addTotalColor() {
-
-  //delclaring color variable
-  var color = $(this).data("color");
-
-  //cycling through to add to correct color total
-  if (color == "red") {
-    redCount++;
-    $("#" + color).text("Total " + color + ": " + redCount);
-
-  } else if (color == "yellow") {
-    yellowCount++;
-    $("#" + color).text("Total " + color + ": " + yellowCount);
-    
-  } else if (color == "green") {
-    greenCount++;
-    $("#" + color).text("Total " + color + ": " + greenCount);
-
-  } else {
-    blueCount++;
-    $("#" + color).text("Total " + color + ": " + blueCount);
-
+  switch (color) {
+    case "red":
+      redCount++;
+      $("#" + color).text("Total " + color + ": " + redCount);
+      break;
+    case "yellow":
+      yellowCount++;
+      $("#" + color).text("Total " + color + ": " + yellowCount);
+      break;
+    case "green":
+      greenCount++;
+      $("#" + color).text("Total " + color + ": " + greenCount);
+      break;
+    case "blue":
+      blueCount++;
+      $("#" + color).text("Total " + color + ": " + blueCount);
+      break;
   }
 }
